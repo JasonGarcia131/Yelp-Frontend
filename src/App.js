1e72084b9bb853a2218cbe907548d0f50ab565fd
Jason Garcia <jaygarcia0530@gmail.com>
2023-06-04 20:59:26 -0700

    set up the frontend routes and layout for home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import Details from './routes/Details';
+import Update from './routes/Update';
 
 function App() {
   return (
-    <div>
+    <div className='container'>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/restaurants/:id' element={<Details/>}/>
+          <Route exact path='/restaurants/:id/update' element={<Update/>}/>
+        </Routes>
+    </Router>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header>
+            <h1 className="font-weight-light display-1 text-center">
+                Restaurant Finder
+            </h1>
+        </header>
+    )
+}
+
+export default Header;
